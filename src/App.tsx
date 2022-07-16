a7257a69c88caecc0a7de717edcf30d13c1a840e
Saumitra Lohokare <saumitra.lohokare@gmail.com>
2022-07-16 23:55:04 +0530

    Added uploading multiple files

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import './App.css';
 function App() {
 
   let [server, setServer] = useState('');
-  let [fileData, setFileData] = useState<File>();
   let [downloadLink, setDownloadLink] = useState('');
+  let [fileList, setFileList] = useState<FileList>();
+
+  /// GOFILE STUFF
 
   const getServer = async () => {
     await fetch('https://api.gofile.io/getServer')
@@ -13,16 +15,9 @@ function App() {
     .then(data => setServer(data.data.server));
   }
 
-  const setFile = (target: HTMLInputElement) => {
-    let file = target.files?.item(0);
-    if (file) {
-      setFileData(file);
-    }
-  }
-
-  const uploadFile = async () => {
-    if (!fileData) {
-      console.log('File not set!');
+  const uploadFiles = async () => {
+    if (!fileList) {
+      console.log('Files not set!');
       return;
     }
 
@@ -32,33 +27,64 @@ function App() {
     }
 
     let data = new FormData();
-    data.append('file', fileData, fileData.name);
+    data.append('file', fileList[0], fileList[0].name);
 
-    await fetch(`https://${server}.gofile.io/uploadFile`,{
+    await fetch(`https://${server}.gofile.io/uploadFile`, {
       method: 'POST',
       body: data
     })
     .then(response => response.json())
-    .then(response => {
-      setDownloadLink(`https://${server}.gofile.io/download/direct/${response.data.fileId}/${response.data.fileName}`);
+    .then(async response => {
+      setDownloadLink(response.data.downloadPage);
+
+      let folderId = response.data.parentFolder;
+      let guestToken = response.data.guestToken;
       console.log(response);
       
+      for (let i = 1; i < fileList!.length; i++) {
+        data = new FormData();
+        let file = fileList!.item(i);
+        if (file && folderId !== '') {
+          data.append('file', file, file.name);
+          data.append('token', guestToken);
+          data.append('folderId', folderId);
+
+          await fetch(`https://${server}.gofile.io/uploadFile`, {
+              method: 'POST',
+              body: data
+          })
+          .then(response => response.json())
+          .then(response => {
+              console.log(response);
+          })
+          .catch(error => console.error(error));
+        }
+      }
     })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error));
   }
 
+  /// ----
+
+  if (!server || server === '') getServer();
+
+  // TODO: Add drag and drop for files.
+  const setFile = (target: HTMLInputElement) => {
+    if (target.files) setFileList(target.files);
+  }
+
+  // TODO: Make good UI.
   return (
     <div className="App">
       <header className="App-header">
         <span>Server is {server}!</span>
-        <button onClick={getServer}>Get Server</button>
         <br />
         <br />
-        <input type="file" name="uploadFile" id="uploadFile" onChange={(e) => setFile((e.target as HTMLInputElement))}/>
-        <button onClick={uploadFile}>Upload File</button>
+        <input type="file" name="uploadFile" id="uploadFile" multiple={true} onChange={(e) => setFile((e.target as HTMLInputElement))}/>
+        <button onClick={uploadFiles}>Upload File</button>
         <br />
         <br />
-        <span>File uploaded can be downloaded at <a href={downloadLink}>{downloadLink}</a></span>
+        <span>File uploaded can be downloaded at <a href={downloadLink} target="_blank" rel="noreferrer">{downloadLink}</a></span>
       </header>
     </div>
   );
