e193f55d499ddf065bdbeb995fadaa4eec2d3b3e
Saumitra Lohokare <saumitra.lohokare@gmail.com>
2022-07-16 15:38:20 +0530

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  let [server, setServer] = useState('');
+  let [fileData, setFileData] = useState<File>();
+  let [downloadLink, setDownloadLink] = useState('');
+
+  const getServer = async () => {
+    await fetch('https://api.gofile.io/getServer')
+    .then(resp => resp.json())
+    .then(data => setServer(data.data.server));
+  }
+
+  const setFile = (target: HTMLInputElement) => {
+    let file = target.files?.item(0);
+    if (file) {
+      setFileData(file);
+    }
+  }
+
+  const uploadFile = async () => {
+    if (!fileData) {
+      console.log('File not set!');
+      return;
+    }
+
+    if (!server || server === '') {
+      console.log('Server not set!');
+      return;
+    }
+
+    let data = new FormData();
+    data.append('file', fileData, fileData.name);
+
+    await fetch(`https://${server}.gofile.io/uploadFile`,{
+      method: 'POST',
+      body: data
+    })
+    .then(response => response.json())
+    .then(response => {
+      setDownloadLink(`https://${server}.gofile.io/download/direct/${response.data.fileId}/${response.data.fileName}`);
+      console.log(response);
+      
+    })
+    .catch(error => console.error(error))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span>Server is {server}!</span>
+        <button onClick={getServer}>Get Server</button>
+        <br />
+        <br />
+        <input type="file" name="uploadFile" id="uploadFile" onChange={(e) => setFile((e.target as HTMLInputElement))}/>
+        <button onClick={uploadFile}>Upload File</button>
+        <br />
+        <br />
+        <span>File uploaded can be downloaded at <a href={downloadLink}>{downloadLink}</a></span>
       </header>
     </div>
   );
